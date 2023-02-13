1078c8b032b48667c7fefb30898364792fc4a4f1
charanjose16 <97113209+charanjose16@users.noreply.github.com>
2023-02-13 09:16:58 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int factorial(int num)
+{
+    if (num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * factorial(num-1);
+    }
+}
+int main()
+{
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("The factorial of %d is %d\n", num, factorial(num));
+    return 0;
+}
